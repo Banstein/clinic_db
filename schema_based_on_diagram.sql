0910115242e94b298a12427ce27c9399f96be5ac
Banstein <islambahnas1@gmail.com>
2022-05-11 10:33:01 +0200

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -26,22 +26,3 @@ CREATE TABLE invoices (
   PRIMARY KEY(id),
   FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id) ON DELETE CASCADE
 );
-
-CREATE TABLE treatments (
-  id INT,
-  type VARCHAR,
-  name VARCHAR,
-  PRIMARY KEY(id),
-  FOREIGN KEY(id) REFERENCES medical_histories(id) ON DELETE CASCADE
-);
-
-CREATE TABLE invoices_items (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  unit_price DECIMAL,
-  quantity INT,
-  total_price DECIMAL,
-  invoice_id INT,
-  treatment_id INT,
-  PRIMARY KEY(id),
-  FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE
-);
